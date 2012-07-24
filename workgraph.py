ac47d02659c57e6e4cd3e1b6279638be94074f66
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2012-07-24 16:44:46 +0200

    Added some formatting and structures

--- workgraph.py
+++ workgraph.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 from datetime import datetime
+
 def parseDateLine(line):
   data = line[2:-1].split(' ')
   start = datetime(1900+int(data[2]), int(data[1]), int(data[0]), int(data[3]), int(data[4]))
   end = datetime(1900+int(data[7]), int(data[6]), int(data[5]), int(data[8]), int(data[9]))
   return {'start': start, 'end': end}
+
 def parseBreakLine(line):
   data = line[2:-1].split(' ')
+  return {}
+
 def parseMiscLine(line):
   data = line[2:-1].split(' ')
+  return {}
+
+days = []
+day = {}
+
 f = open('stats/historystats', 'r')
 for line in f:
   if line[0] == 'D':
-    print parseDateLine(line)
+    days.append(day)
+    day = parseDateLine(line)
   elif line[0] == 'B':
-    parseBreakLine(line)
+    day.update(parseBreakLine(line))
   elif line[0] == 'm':
-    parseMiscLine(line)
+    day.update(parseMiscLine(line))
 f.close()
+
+print days
