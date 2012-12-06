b48a68116a0b2b2024955e4bacfe2e9c8e7d3b04
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2012-12-06 09:14:26 +0200

    Cleaned up code

--- workgraph.py
+++ workgraph.py
@@ -4,15 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 keystrokes=[]
 clicks=[]
-days=[]
 f=open('/home/brett/.workrave/historystats', 'r')
 for line in f:
-  if line[0]=='D':
+  if line[0]=='m':
     # Ignore 'm ' and '\n'
-    data=line[2:-1].split(' ')
-    day=date(1900+int(data[2]), int(data[1])+1, int(data[0]))
-    days.append(day)
-  elif line[0]=='m':
     clicks.append(int(line[2:-1].split(' ')[5]))
     keystrokes.append(int(line[2:-1].split(' ')[6]))
 f.close()
@@ -23,10 +18,9 @@ plt.plot(clicks, 'r--')
 coeff=np.lib.polyfit(range(len(keystrokes)), keystrokes, 1)
 p=np.lib.poly1d(coeff)
 x_array=range(len(keystrokes))
-y_array=p(x_array)
-plt.plot(x_array, y_array, 'g--')
+plt.plot(x_array, p(x_array), 'g--')
 # Labels
 plt.ylabel("Count")
-plt.xlabel("Date")
-plt.legend(("Key presses", "Mouse clicks"), "upper right")
+plt.xlabel("Days")
+plt.legend(("Keystrokes", "Clicks"), "upper right")
 plt.show()
